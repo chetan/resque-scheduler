c5c0bf611642176a9aa5cc93c06b8327f547a21d
Chetan Sarva <chetan@pixelcop.net>
2011-11-29 15:37:20 -0500

    use a distributed lock to avoid a race condition in clean_up_timestamp()

--- lib/resque_scheduler.rb
+++ lib/resque_scheduler.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'resque'
+require 'redis-lock'
 require 'resque/server'
 require 'resque_scheduler/version'
 require 'resque/scheduler'
@@ -16,7 +17,7 @@ module ResqueScheduler
   #                  "description" => "this thing works it"s butter off"},
   #    ...}
   #
-  # 'some_name' can be anything and is used only to describe and reference 
+  # 'some_name' can be anything and is used only to describe and reference
   # the scheduled job
   #
   # :cron can be any cron scheduling string :job can be any resque job class
@@ -48,12 +49,12 @@ module ResqueScheduler
   def schedule
     @schedule ||= {}
   end
-  
+
   # reloads the schedule from redis
   def reload_schedule!
     @schedule = get_schedules
   end
-  
+
   # gets the schedule as it exists in redis
   def get_schedules
     if redis.exists(:schedules)
@@ -66,12 +67,12 @@ module ResqueScheduler
       nil
     end
   end
-  
+
   # Create or update a schedule with the provided name and configuration.
   #
   # Note: values for class and custom_job_class need to be strings,
   # not constants.
-  #  
+  #
   #    Resque.set_schedule('some_job', {:class => 'SomeJob',
   #                                     :every => '15mins',
   #                                     :queue => 'high',
@@ -84,12 +85,12 @@ module ResqueScheduler
     end
     config
   end
-  
+
   # retrive the schedule configuration for the given name
   def get_schedule(name)
     decode(redis.hget(:schedules, name))
   end
-  
+
   # remove a given schedule by name
   def remove_schedule(name)
     redis.hdel(:schedules, name)
@@ -106,16 +107,16 @@ module ResqueScheduler
   end
 
   # Identical to +enqueue_at+, except you can also specify
-  # a queue in which the job will be placed after the 
+  # a queue in which the job will be placed after the
   # timestamp has passed.
   def enqueue_at_with_queue(queue, timestamp, klass, *args)
     before_hooks = before_schedule_hooks(klass).collect do |hook|
       klass.send(hook,*args)
     end
     return false if before_hooks.any? { |result| result == false }
-    
+
     delayed_push(timestamp, job_to_hash_with_queue(queue, klass, args))
-    
+
     after_schedule_hooks(klass).collect do |hook|
       klass.send(hook,*args)
     end
@@ -128,7 +129,7 @@ module ResqueScheduler
   end
 
   # Identical to +enqueue_in+, except you can also specify
-  # a queue in which the job will be placed after the 
+  # a queue in which the job will be placed after the
   # number of seconds has passed.
   def enqueue_in_with_queue(queue, number_of_seconds_from_now, klass, *args)
     enqueue_at_with_queue(queue, Time.now + number_of_seconds_from_now, klass, *args)
@@ -139,13 +140,16 @@ module ResqueScheduler
   # Insertion if O(log(n)).
   # Returns true if it's the first job to be scheduled at that time, else false
   def delayed_push(timestamp, item)
-    # First add this item to the list for this timestamp
-    redis.rpush("delayed:#{timestamp.to_i}", encode(item))
+    redis.lock_for_update(:resque_scheduler_crud) do
 
-    # Now, add this timestamp to the zsets.  The score and the value are
-    # the same since we'll be querying by timestamp, and we don't have
-    # anything else to store.
-    redis.zadd :delayed_queue_schedule, timestamp.to_i, timestamp.to_i
+      # First add this item to the list for this timestamp
+      redis.rpush("delayed:#{timestamp.to_i}", encode(item))
+
+      # Now, add this timestamp to the zsets.  The score and the value are
+      # the same since we'll be querying by timestamp, and we don't have
+      # anything else to store.
+      redis.zadd :delayed_queue_schedule, timestamp.to_i, timestamp.to_i
+    end
   end
 
   # Returns an array of timestamps based on start and count
@@ -196,15 +200,17 @@ module ResqueScheduler
 
   # Clears all jobs created with enqueue_at or enqueue_in
   def reset_delayed_queue
-    Array(redis.zrange(:delayed_queue_schedule, 0, -1)).each do |item|
-      redis.del "delayed:#{item}"
-    end
+    redis.lock_for_update(:resque_scheduler_crud) do
+      Array(redis.zrange(:delayed_queue_schedule, 0, -1)).each do |item|
+        redis.del "delayed:#{item}"
+      end
 
-    redis.del :delayed_queue_schedule
+      redis.del :delayed_queue_schedule
+    end
   end
 
   # Given an encoded item, remove it from the delayed_queue
-  # 
+  #
   # This method is potentially very expensive since it needs to scan
   # through the delayed queue for every timestamp.
   def remove_delayed(klass, *args)
@@ -215,7 +221,7 @@ module ResqueScheduler
     end
     destroyed
   end
-  
+
   # Given a timestamp and job (klass + args) it removes all instances and
   # returns the count of jobs removed.
   #
@@ -232,25 +238,27 @@ module ResqueScheduler
     total_jobs = 0
     Array(redis.zrange(:delayed_queue_schedule, 0, -1)).each do |timestamp|
       total_jobs += redis.llen("delayed:#{timestamp}").to_i
-    end 
+    end
     total_jobs
-  end 
+  end
 
   private
-  
+
     def job_to_hash(klass, args)
       {:class => klass.to_s, :args => args, :queue => queue_from_class(klass)}
     end
-    
+
     def job_to_hash_with_queue(queue, klass, args)
       {:class => klass.to_s, :args => args, :queue => queue}
     end
 
     def clean_up_timestamp(key, timestamp)
-      # If the list is empty, remove it.
-      if 0 == redis.llen(key).to_i
-        redis.del key
-        redis.zrem :delayed_queue_schedule, timestamp.to_i
+      redis.lock_for_update(:resque_scheduler_crud) do
+        # If the list is empty, remove it.
+        if 0 == redis.llen(key).to_i
+          redis.del key
+          redis.zrem :delayed_queue_schedule, timestamp.to_i
+        end
       end
     end
 
